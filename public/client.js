fe80ab5eec99beec154dc59333497dded361cbb8
Yekaterina Jalette <doroskaterina@gmail.com>
2018-01-21 15:58:16 -0500

    fixing web responsiveness

---
+++ public/client.js
@@ -0,0 +1,17 @@
+//$('main').not('.home-page, .signup-page').toggle();
+//
+//$("#button-signin").click(function () {
+//    $('main').not('.account-dashboard-page').toggle();
+//});
+//
+//
+//
+//$("#button-signup").click(function () {
+//    $('main').not('.create-a-link-page').toggle();
+//});
+//
+//
+//$("#button-create-form").click(function () {
+//    alert("Your link was saved!");
+//    $('main').not('.account-dashboard-page').toggle();
+//});

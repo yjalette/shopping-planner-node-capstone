622a85dd148cb68383591bc1fe07748e253b4961
Yekaterina Doros <yekaterinadoros@gmail.com>
2018-11-27 21:32:55 -0500

    animation

---
+++ public/index.js
@@ -0,0 +1,58 @@
+$(".button-learn").click(function() {
+    $(".signup-page").show();
+    $(".middle-home-page").hide();
+});
+$(".button-learn").click(function() {
+    $('html,body').animate({
+            scrollTop: $(".signup-page").offset().top
+},'slow');
+});
+
+$("#link-it-button").click(function() {
+    $(".middle-home-page").show();
+    $(".signup-page").hide();
+});
+$("#link-it-button").click(function() {
+    $('html,body').animate({
+            scrollTop: $(".middle-home-page").offset().top
+},'slow');
+});
+
+// set up text to print, each item in array is new line
+var aText = new Array(
+"Save your time. Link It"
+
+);
+var iSpeed = 100; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+
+function typewriter() {
+    sContents = ' ';
+    iRow = Math.max(0, iIndex - iScrollAt);
+    var destination = document.getElementById("typedtext");
+
+    while (iRow < iIndex) {
+        sContents += aText[iRow++] + '<br />';
+    }
+
+    destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+
+    if (iTextPos++ == iArrLength) {
+        iTextPos = 0;
+        iIndex++;
+    if (iIndex != aText.length) {
+        iArrLength = aText[iIndex].length;
+        setTimeout("typewriter()", 500);
+    }
+    } else {
+        setTimeout("typewriter()", iSpeed);
+    }
+}
+
+typewriter();
